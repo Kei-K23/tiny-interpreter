bbc7eeea4965707801e214febd1c24a1894ad503
Kei-K23 <keiksl2301@gmail.com>
2024-08-30 15:26:26 +0630

    Add basic Lexer to generate tokens that need by parser

--- main.cpp
+++ main.cpp
@@ -27,8 +27,121 @@ struct Token
     std::string value;
 };
 
+class Lexer
+{
+public:
+    Lexer(std::string &source) : source(source), pos(0), currentChar(source[0]) {}
+
+    Token getNextToken()
+    {
+        while (currentChar != '\0')
+        {
+            if (isspace(currentChar))
+            {
+                advance();
+                continue;
+            }
+
+            if (isdigit(currentChar))
+            {
+                return number();
+            }
+
+            if (isalpha(currentChar))
+            {
+                return identifier();
+            }
+
+            if (currentChar == '+')
+            {
+                return {TokenType::PLUS, "+"};
+            }
+
+            if (currentChar == '-')
+            {
+                return {TokenType::MINUS, "-"};
+            }
+
+            if (currentChar == '*')
+            {
+                return {TokenType::MULTIPLY, "*"};
+            }
+
+            if (currentChar == '/')
+            {
+                return {TokenType::DIVIDE, "/"};
+            }
+
+            if (currentChar == '=')
+            {
+                return {TokenType::ASSIGN, "="};
+            }
+
+            if (currentChar == ';')
+            {
+                return {TokenType::SEMICOLON, ";"};
+            }
+
+            if (currentChar == '(')
+            {
+                return {TokenType::L_PAREN, "("};
+            }
+
+            if (currentChar == ')')
+            {
+                return {TokenType::R_PAREN, ")"};
+            }
+
+            return {TokenType::INVALID, std::string(1, currentChar)};
+        }
+        return {TokenType::END_OF_LINE, ""};
+    }
+
+private:
+    std::string source;
+    size_t pos;
+    char currentChar;
+
+    void advance()
+    {
+        pos++;
+        if (pos > source.length())
+        {
+            currentChar = source[pos];
+        }
+        else
+        {
+            currentChar = '\0'; // Reach to the end of file
+        }
+    }
+
+    Token number()
+    {
+        std::string result;
+        while (isdigit(currentChar))
+        {
+            result += currentChar;
+            advance();
+        }
+        return {TokenType::NUMBER, result};
+    }
+
+    Token identifier()
+    {
+        std::string result;
+        while (isalnum(currentChar))
+        {
+            result += currentChar;
+            advance();
+        }
+        return {TokenType::IDENTIFIER, result};
+    }
+};
+
 // Main Program
 int main()
 {
+    std::string source = "1 + 2 - 3;";
+    Lexer lexer(source);
     return 0;
 }
