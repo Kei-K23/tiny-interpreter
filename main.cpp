267c119ad46ae21b432a864fe82e161ea6e06e16
Kei-K23 <keiksl2301@gmail.com>
2024-08-30 15:08:28 +0630

    Add TokenType enum

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <string>
 
+// Token and Lexer Definitions
+enum class TokenType
+{
+    IDENTIFIER,
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    ASSIGN,
+    SEMICOLON,
+    L_PAREN,
+    R_PAREN,
+    END_OF_LINE,
+    INVALID
+};
+
+struct Token
+{
+    TokenType type;
+    std::string value;
+};
+
+// Main Program
 int main()
 {
-    std::cout << "Hello" << std::endl;
     return 0;
 }
